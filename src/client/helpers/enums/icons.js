1ef0773e13cd4768f3b81d9fa939309b28627f1e
Jill Magsaysay <jill@mondorobot.com>
2017-07-08 20:30:20 -0600

    updating about page

--- src/client/helpers/enums/icons.js
+++ src/client/helpers/enums/icons.js
@@ -1,24 +1,34 @@
 import IconAccount from 'icons/icon-account';
+import IconBall from 'icons/icon-ball';
+import IconBeer from 'icons/icon-beer';
 import IconBriefcase from 'icons/icon-briefcase';
 import IconBuilding from 'icons/icon-building';
 import IconCode from 'icons/icon-code';
+import IconFood from 'icons/icon-food';
 import IconGlasses from 'icons/icon-glasses';
+import IconKeyboard from 'icons/icon-keyboard';
 import IconLaunch from 'icons/icon-launch';
 import IconLightbulb from 'icons/icon-lightbulb';
 import IconMenu from 'icons/icon-menu';
 import IconPartner from 'icons/icon-partner';
+import IconThumbsUp from 'icons/icon-thumbs-up';
 import IconX from 'icons/icon-x';
 
 const Icons = {
   'account': IconAccount,
+  'ball': IconBall,
+  'beer': IconBeer,
   'briefcase': IconBriefcase,
   'building': IconBuilding,
   'code': IconCode,
+  'food': IconFood,
   'glasses': IconGlasses,
+  'keyboard': IconKeyboard,
   'launch': IconLaunch,
   'lightbulb': IconLightbulb,
   'menu': IconMenu,
   'partner': IconPartner,
+  'thumbs-up': IconThumbsUp,
   'x': IconX
 };
 
--- src/client/pages/about/about.jsx
+++ src/client/pages/about/about.jsx
@@ -1,9 +1,46 @@
 import React, {Component} from 'react';
+import ListItem from 'components/list-item';
 import Page from 'components/page';
+import Text from 'components/text';
 
 export default class About extends Component {
   constructor () {
     super();
+
+    this.generateData();
+  }
+
+  generateData() {
+    this.professionalData = [
+      {
+        'avatar': 'keyboard',
+        'primaryContent': ``,
+        'title': 'Software Engineer'
+      },
+      {
+        'avatar': 'thumbs-up',
+        'primaryContent': ``,
+        'title': 'Mentor'
+      }
+    ];
+
+    this.personalData = [
+      {
+        'avatar': 'ball',
+        'primaryContent': ``,
+        'title': 'Athlete'
+      },
+      {
+        'avatar': 'food',
+        'primaryContent': ``,
+        'title': 'Amateur Chef'
+      },
+      {
+        'avatar': 'beer',
+        'primaryContent': ``,
+        'title': 'Homebrewer'
+      }
+    ];
   }
 
   render() {
@@ -13,7 +50,36 @@ export default class About extends Component {
         heroTitle="Who am I?"
         heroSupportingTxt="Softball player/coach, sports enthusiast, beer lover, amateur chef, mentor, and software engineer"
       >
-         Coming soon, please check back soon...
+        <Text type="display-sm" underlined>Professional</Text>
+        <div className="grid">
+          {this.professionalData.map((listItem, i) => {
+            return(
+              <div className="grid__col" key={`list-item-${i}`}>
+                <ListItem
+                  externalLink={listItem.url}
+                  avatar={listItem.avatar}
+                  title={listItem.title}
+                  primaryContent={listItem.primaryContent}
+                />
+            </div>
+            );
+          })}
+        </div>
+        <Text type="display-sm" underlined>Personal</Text>
+        <div className="grid">
+          {this.personalData.map((listItem, i) => {
+            return(
+              <div className="grid__col" key={`list-item-${i}`}>
+                <ListItem
+                  externalLink={listItem.url}
+                  avatar={listItem.avatar}
+                  title={listItem.title}
+                  primaryContent={listItem.primaryContent}
+                />
+            </div>
+            );
+          })}
+        </div>
       </Page>
     );
   }
