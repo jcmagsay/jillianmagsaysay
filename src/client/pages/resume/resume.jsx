9db0cbc2cac6e90ff0788d6fdad775d4d612783c
Jill Magsaysay <jill@mondorobot.com>
2017-07-08 21:19:16 -0600

    Updating Resume Page

--- src/client/pages/resume/resume.jsx
+++ src/client/pages/resume/resume.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Page from 'components/page';
+import Text from 'components/text';
 
 export default class Resume extends Component {
   constructor () {
@@ -13,7 +14,14 @@ export default class Resume extends Component {
         heroTitle="Software Engineer"
         heroSupportingTxt="If you want to be formal"
       >
-        Coming soon, please check back soon...
+        <Text type="headline" align="centered">
+          ***CV is currently out of date and will be updated soon.***
+        </Text>
+        <div className="grid">
+          <div className="grid__col">
+            <img width="90%" src={require('media/images/cv.png')} alt="Jill Resume" />
+          </div>
+        </div>
       </Page>
     );
   }
