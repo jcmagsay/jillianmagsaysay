aaeb07eeb0aed010297b0d9276d3d5fa67264b3c
Jill Magsaysay <jcmagsay@gmail.com>
2019-01-17 10:02:07 -0700

    refactor pages: ideas, not-found, projects, resume. updated resume to current

--- src/client/pages/ideas/ideas.jsx
+++ src/client/pages/ideas/ideas.jsx
@@ -2,31 +2,25 @@ import React, {Component} from 'react';
 import Page from 'components/page';
 import Text from 'components/text';
 
-export default class Ideas extends Component {
-  constructor () {
-    super();
+const introduction = `My Idea Bucket is a place where I can experiment and use my creativity without limits or boundaries.`
+const description = `At work, we're all held back by timelines and budget constraints. At home, I can determine when a project is complete, experiment with new technologies, and use my talents at maximum potential.`;
 
-    this.introduction = `My Idea Bucket is a place where I can experiment and use my creativity without limits or boundaries.`
-    this.description = `At work, we're all held back by timelines and budget constraints. At home, I can determine when a project is complete, experiment with new technologies, and use my talents at maximum potential.`;
-  }
+const Ideas = () => (
+  <Page
+    heroImage={require('media/images/ideas.png')}
+    heroTitle="Idea Bucket"
+    heroSupportingTxt="Knowledge is not power. It's only potential power. Action is power."
+  >
+    <Text type="headline">
+      {this.introduction}
+    </Text>
+    <Text type="headline">
+      {this.description}
+    </Text>
+    <Text type="subheading">
+      Check back soon to view my recent experiments.
+    </Text>
+  </Page>
+);
 
-  render() {
-    return (
-      <Page
-        heroImage={require('media/images/ideas.png')}
-        heroTitle="Idea Bucket"
-        heroSupportingTxt="Knowledge is not power. It's only potential power. Action is power."
-      >
-        <Text type="headline">
-          {this.introduction}
-        </Text>
-        <Text type="headline">
-          {this.description}
-        </Text>
-        <Text type="subheading">
-          Check back soon to view my recent experiments.
-        </Text>
-      </Page>
-    );
-  }
-}
+export default Ideas;
