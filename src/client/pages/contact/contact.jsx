60487939a2f4dce76b87bda6100c08746e90b551
Jill Magsaysay <jcmagsay@gmail.com>
2019-01-17 08:47:19 -0700

    refactoring home page

--- src/client/pages/contact/contact.jsx
+++ src/client/pages/contact/contact.jsx
@@ -3,59 +3,51 @@ import ListItem from 'components/list-item';
 import Page from 'components/page';
 import Text from 'components/text';
 
-export default class Contact extends Component {
-  constructor () {
-    super();
-
-    this.generateData();
+const listItems = [
+  {
+    'avatar': 'building',
+    'primaryContent': `Looking for a Senior Software Engineer at your company?`,
+    'title': 'Employer'
+  },
+  {
+    'avatar': 'glasses',
+    'primaryContent': `Need to get a customized site up and running in no time?`,
+    'title': 'Freelance',
+  },
+  {
+    'avatar': 'partner',
+    'primaryContent': 'Need a Front-End specialist to collaborate on a project?',
+    'title': 'Partner'
   }
+];
 
-  generateData() {
-    this.listItems = [
-      {
-        'avatar': 'building',
-        'primaryContent': `Looking for a Senior Software Engineer at your company?`,
-        'title': 'Employer'
-      },
-      {
-        'avatar': 'glasses',
-        'primaryContent': `Need to get a customized site up and running in no time?`,
-        'title': 'Freelance',
-      },
-      {
-        'avatar': 'partner',
-        'primaryContent': 'Need a Front-End specialist to collaborate on a project?',
-        'title': 'Partner'
-      }
-    ];
-  }
+const Contact = () => {
+  return (
+    <Page
+      heroImage={require('media/images/chomps.png')}
+      heroTitle="Contact"
+      heroSupportingTxt="Have a bone to pick?"
+    >
+      <div className="grid">
+        {listItems.map((listItem, i) => {
+          return(
+            <div className="grid__col" key={`list-item-${i}`}>
+              <ListItem
+                externalLink={listItem.url}
+                avatar={listItem.avatar}
+                title={listItem.title}
+                primaryContent={listItem.primaryContent}
+              />
+          </div>
+          );
+        })}
+      </div>
+      <Text type="headline" align="centered">
+        <div>I'd love to hear from you!</div>
+        Shoot me an email at <a href="mailto:jcmagsay@gmail.com">jcmagsay@gmail.com</a>
+      </Text>
+    </Page>
+  );
+};
 
-  render() {
-    return (
-      <Page
-        heroImage={require('media/images/chomps.png')}
-        heroTitle="Contact"
-        heroSupportingTxt="Have a bone to pick?"
-      >
-        <div className="grid">
-          {this.listItems.map((listItem, i) => {
-            return(
-              <div className="grid__col" key={`list-item-${i}`}>
-                <ListItem
-                  externalLink={listItem.url}
-                  avatar={listItem.avatar}
-                  title={listItem.title}
-                  primaryContent={listItem.primaryContent}
-                />
-            </div>
-            );
-          })}
-        </div>
-        <Text type="headline" align="centered">
-          <div>I'd love to hear from you!</div>
-          Shoot me an email at <a href="mailto:jcmagsay@gmail.com">jcmagsay@gmail.com</a>
-        </Text>
-      </Page>
-    );
-  }
-}
+export default Contact;
